25e6c4111bd92048ac9624ac0013b4b1897e1077
jaman1000 <42631742+jaman1000@users.noreply.github.com>
2021-11-03 18:55:30 -0400

    Initial React Natvie set up
    Login Screen UI

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { createAppContainer, StackNavigator } from 'react-navigation';
+import Login from "./src/Screens/Login/login";
+import { STATEMENT_OR_BLOCK_KEYS } from "@babel/types";
+
+const Stack = createStackNavigator()
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen name='LoginScreen' component={Login} />
+       
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
